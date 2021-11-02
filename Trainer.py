3fb869670a41d8181d691b2646ddd1160b7230b2
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2021-11-02 20:10:33 +0100

    eksperymenty

--- Trainer.py
+++ Trainer.py
@@ -41,14 +41,78 @@ def epoch(network: Net.Network, training_set, alpha=0.6):
         layer.weights = layer.weights - alpha / len(training_set) * sum([el[l] for el in mini_bath_gradients])
         layer.bia = layer.bias - alpha / len(training_set) * sum([el[l] for el in mini_bath_errors])
 
-def train_network(network, training_set, batch_size):
+
+def iteration(network, training_set, batch_size, mini_batches_count, alpha=0.6):
+    index = 0
+    last_index = min(batch_size, len(training_set))
+    for i in range(mini_batches_count):
+        epoch(network, training_set[index:last_index], alpha)
+        index = last_index
+        last_index = min(last_index + batch_size, len(training_set))
+
+
+def train_network(network: Net.Network, training_set, validation_set, batch_size, iterations=8, alpha=0.6):
     mini_batches_count = -(len(training_set) // -batch_size)
+    errors = []
+    for i in range(iterations):
+        iteration(network, training_set, batch_size, mini_batches_count, alpha)
+        val_err = error(network, validation_set)
+        errors.append(val_err)
+        print(val_err)
+    return errors
+
+
+def acc(network, data_set):
+    correct = 0
+    for image, label in data_set:
+        network.process_input(image)
+        correct = correct + (label[get_result(network.output)] == 1)
+    return correct / len(data_set)
+
+
+def error(network, data_set):
+    return 1 - acc(network, data_set)
+
+
+def early_return_iteration(network: Net.Network, training_set, validation_set, batch_size, mini_batches_count, max_val_error_increase, prev_weights, prev_validation_error):
     index = 0
     last_index = min(batch_size, len(training_set))
     for i in range(mini_batches_count):
         epoch(network, training_set[index:last_index])
+
+        validation_error = error(network, validation_set)
+        if validation_error > prev_validation_error + max_val_error_increase:
+            print('doopa')
+            for i, old_weights in enumerate(prev_weights):
+                network.layers[i].weights = old_weights
+        else:
+            prev_validation_error = validation_error
+            prev_weights = [layer.weights for layer in network.layers]
+
         index = last_index
         last_index = min(last_index + batch_size, len(training_set))
+
+    return prev_weights, prev_validation_error
+
+
+def early_return_training(network, training_set, validation_set, batch_size, max_val_error_increase=0.05, iterations=8):
+    mini_batches_count = -(len(training_set) // -batch_size)
+    starting_validation_error = error(network, validation_set)
+    starting_weights = [layer.weights for layer in network.layers]
+
+    prev_weights, prev_validation_error = early_return_iteration(network, training_set, validation_set, batch_size,
+                                                                 mini_batches_count, max_val_error_increase,
+                                                                 starting_weights, starting_validation_error)
+
+    errors = [prev_validation_error]
+    for i in range(1, iterations):
+        print(prev_validation_error)
+        prev_weights, prev_validation_error = early_return_iteration(network, training_set, validation_set, batch_size,
+                                                                     mini_batches_count, max_val_error_increase,
+                                                                     starting_weights, starting_validation_error)
+        errors.append(prev_validation_error)
+    print(errors)
+
 
 
 def get_result(input):
@@ -70,7 +134,7 @@ def Test():
     expected5 = np.array([0, 1]).reshape((2, 1))
     expected = [expected1, expected2, expected3, expected4, expected5]
     training_set = [list(a) for a in zip(data, expected)]
-    train_network(net, training_set, 3)
+    iteration(net, training_set, 3)
     get_result(np.array([2, 3, 1, 0]).reshape((4, 1)))
     #epoch(net, training_set)
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 from mnist import MNIST
-from matplotlib import pyplot as plt
 import Network as Net
 import Layer as Nl
 import Trainer as Tr
@@ -25,10 +24,8 @@ def prepare_training_set():
         images_normalized.append(image.reshape(784, 1))
 
     training_set = [list(a) for a in zip(images_normalized, labels_n_to_1)]
-
-    #print(training_set[0])
-
     return training_set
+
 
 def prepare_validation_set():
     mndata = MNIST('./dataset')
@@ -50,38 +47,209 @@ def prepare_validation_set():
 
     training_set = [list(a) for a in zip(images_normalized, labels_n_to_1)]
 
-    print(labels.size)
-
     return training_set
 
 
-def nyan():
+def early_test():
     training_set = prepare_training_set()
     validation_set = prepare_validation_set()
-    network = Net.Network(input_size=784, sizes=[30, 14], activation_functions=[Nl.sigmoid, Nl.ReLU],
-                          derivatives=[Nl.sigmoid_derivative, Nl.ReLu_derivative], output_size=10, standard_deviation=0.3)
-    print("zabawa rozpoczeta")
-    for i in range(10):
-        print(i)
-        Tr.train_network(network, training_set, 600)
-        sum = 0
-        for image, label in validation_set:
-            network.process_input(image)
-            sum = sum + (label[Tr.get_result(network.output)] == 1)
-        print(sum / len(training_set))
-
-    sum = 0
-    for image, label in training_set:
-        network.process_input(image)
-        sum = sum + (label[Tr.get_result(network.output)] == 1)
-
-    print(sum / len(training_set))
+    network = Net.Network(input_size=784, sizes=[30, 14], activation_functions=[Nl.sigmoid, Nl.sigmoid],
+                          derivatives=[Nl.sigmoid_derivative, Nl.sigmoid_derivative], output_size=10,
+                          standard_deviation=0.3)
+    Tr.early_return_training(network, training_set, validation_set, 600)
 
 
+def test_alpha():
+    training_set = prepare_training_set()
+    validation_set = prepare_validation_set()
+    iterations_count = 6
 
-# Press the green button in the gutter to run the script.
+    def single_experiment(alpha):
+        network = Net.Network(input_size=784, sizes=[30, 14], activation_functions=[Nl.sigmoid, Nl.sigmoid],
+                              derivatives=[Nl.sigmoid_derivative, Nl.sigmoid_derivative], output_size=10,
+                              standard_deviation=0.3)
+        return Tr.train_network(network, training_set, validation_set, batch_size=600, alpha=alpha, iterations=iterations_count)
+
+    def single_alpha_experiment(alpha):
+        single_alpha_results = []
+        for i in range(10):
+            single_alpha_results.append(single_experiment(alpha))
+        return np.array(single_alpha_results).mean(axis=1)
+
+    alphas = [0.4, 0.5, 0.6, 0.7]
+    results = []
+    for alpha in alphas:
+        results.append(single_experiment(alpha))
+
+    def results_to_text(results, iterations):
+        text = ""
+        for iteration in range(iterations):
+            text = text + f"{results[0][iteration]}"
+            for result in results[1::]:
+                text = text + f";{result[iteration]}"
+            text = text + "\n"
+        return text
+    return results_to_text(results, iterations_count)
+
+
+def test_mini_batch():
+    training_set = prepare_training_set()
+    validation_set = prepare_validation_set()
+    alpha = 0.7
+    iterations_count = 10
+
+    def single_experiment(batch_size):
+        network = Net.Network(input_size=784, sizes=[30, 14], activation_functions=[Nl.sigmoid, Nl.sigmoid],
+                              derivatives=[Nl.sigmoid_derivative, Nl.sigmoid_derivative], output_size=10,
+                              standard_deviation=0.3)
+        return Tr.train_network(network, training_set, validation_set, batch_size=batch_size, alpha=alpha, iterations=iterations_count)
+
+    def single_batch_experiment(batch_size):
+        single_alpha_results = []
+        for i in range(10):
+            single_alpha_results.append(single_experiment(batch_size))
+        return np.array(single_alpha_results).mean(axis=1)
+
+    batch_sizes = [6000, 12000, 18000, 24000]
+    results = []
+    for batch_size in batch_sizes:
+        results.append(single_experiment(batch_size))
+
+    def results_to_text(results, iterations):
+        text = ""
+        for iteration in range(iterations):
+            text = text + f"{iteration + 1};"
+            for result in results:
+                text = text + f";{result[iteration]}"
+            text = text + "\n"
+        return text
+    return results_to_text(results, iterations_count)
+
+
+def test_standard_deviation():
+    training_set = prepare_training_set()
+    validation_set = prepare_validation_set()
+    alpha = 0.7
+    iterations_count = 6
+
+    def single_experiment(standard_deviation):
+        network = Net.Network(input_size=784, sizes=[30, 14], activation_functions=[Nl.sigmoid, Nl.sigmoid],
+                              derivatives=[Nl.sigmoid_derivative, Nl.sigmoid_derivative], output_size=10,
+                              standard_deviation=standard_deviation)
+        return Tr.train_network(network, training_set, validation_set, batch_size=600, alpha=alpha, iterations=iterations_count)
+
+    def single_batch_experiment(standard_deviation):
+        single_alpha_results = []
+        for i in range(10):
+            single_alpha_results.append(single_experiment(standard_deviation))
+        return np.array(single_alpha_results).mean(axis=1)
+
+    deviations = [0.1, 0.2, 0.3, 0.4]
+    results = []
+    for batch_size in deviations:
+        results.append(single_experiment(batch_size))
+
+    def results_to_text(results, iterations):
+        text = "Pomiar;"
+        for dev in deviations:
+            text = text + f";odchylenie={dev}"
+        for iteration in range(iterations):
+            text = text + f"{iteration + 1}"
+            for result in results:
+                text = text + f";{result[iteration]}"
+            text = text + "\n"
+        return text
+    return results_to_text(results, iterations_count)
+
+
+def test_activation_functions():
+    training_set = prepare_training_set()
+    validation_set = prepare_validation_set()
+    alpha = 0.7
+    iterations_count = 6
+
+    def single_experiment(activation_functions, derivatives):
+        network = Net.Network(input_size=784, sizes=[30, 14], activation_functions=activation_functions,
+                              derivatives=derivatives, output_size=10,
+                              standard_deviation=0.3)
+        return Tr.train_network(network, training_set, validation_set, batch_size=600, alpha=alpha, iterations=iterations_count)
+
+    def single_batch_experiment(activation_functions, derivatives):
+        single_alpha_results = []
+        for i in range(10):
+            single_alpha_results.append(single_experiment(activation_functions, derivatives))
+        return np.array(single_alpha_results).mean(axis=1)
+
+    act = [[Nl.ReLU, Nl.ReLU], [Nl.sigmoid, Nl.ReLU]]
+    der = [[Nl.ReLu_derivative, Nl.ReLu_derivative], [Nl.sigmoid_derivative, Nl.ReLu_derivative]]
+    results = []
+    for activations, der in zip(act, der):
+        results.append(single_experiment(activations, der))
+
+    def results_to_text(results, iterations):
+        text = "Pomiar;reLu-ReLU;Sigmoid-ReLU"
+        for iteration in range(iterations):
+            text = text + f"{iteration + 1}"
+            for result in results:
+                text = text + f";{result[iteration]}"
+            text = text + "\n"
+        return text
+    return results_to_text(results, iterations_count)
+
+
+def test_layer_sizes():
+    training_set = prepare_training_set()
+    validation_set = prepare_validation_set()
+    alpha = 0.7
+    iterations_count = 6
+
+    def single_experiment(sizes):
+        network = Net.Network(input_size=784, sizes=sizes, activation_functions=[Nl.ReLU, Nl.ReLU],
+                              derivatives=[Nl.ReLu_derivative, Nl.ReLu_derivative], output_size=10,
+                              standard_deviation=0.3)
+        return Tr.train_network(network, training_set, validation_set, batch_size=600, alpha=alpha, iterations=iterations_count)
+
+    def single_batch_experiment(sizes):
+        single_alpha_results = []
+        for i in range(10):
+            single_alpha_results.append(single_experiment(sizes))
+        return np.array(single_alpha_results).mean(axis=1)
+
+    layer_sizes = [[120, 40], [60, 40]]
+    results = []
+    for l_sizes in layer_sizes:
+        results.append(single_experiment(l_sizes))
+
+    def results_to_text(results, iterations):
+        text = "Pomiar"
+        for l_sizes in layer_sizes:
+            text = text + f";{l_sizes}"
+        for iteration in range(iterations):
+            text = text + f"{iteration + 1}"
+            for result in results:
+                text = text + f";{result[iteration]}"
+            text = text + "\n"
+        return text
+    return results_to_text(results, iterations_count)
+
+
+def some_testing():
+    a = np.array([[1, 2], [2, 3]])
+    print(a[0][0])
+    print(a[1::])
+    print(np.mean(a, axis=1))
+
 if __name__ == '__main__':
-    nyan()
-    #prepare_training_set()
+    #some_testing()
+    #text = test_alpha()
+    #text = test_standard_deviation()
+    #text = test_activation_functions()
+    text = test_layer_sizes()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    with open("Output.txt", "w") as text_file:
+        text_file.write(text)
+
+
+
+
